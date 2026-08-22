0b5d21482d487e4f8d8ef993c65f694101b9f3ab
nafitv24-web <nafitv24@gmail.com>
2026-08-22 19:34:46 +0600

    Bump version code to 27 and version name to 2.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.aistudio.nafitv.ifmvfb"
     minSdk = 24
     targetSdk = 36
-    versionCode = 26
-    versionName = "2.5.4"
+    versionCode = 27
+    versionName = "2.5.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
