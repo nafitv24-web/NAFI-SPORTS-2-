91bc5f3040bc4d292a91d0f9045f86096b671ead
nafitv24-web <nafitv24@gmail.com>
2026-09-16 08:05:32 +0600

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.aistudio.nafitv.ifmvfb"
     minSdk = 24
     targetSdk = 36
-    versionCode = 23
-    versionName = "2.6.5"
+    versionCode = 25
+    versionName = "2.6.7"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
